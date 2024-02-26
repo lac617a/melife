954c51e6bc0bda9cb791107a46c663ce7cff509f
lac617a <botlacrita617@gmail.com>
2024-02-26 18:43:44 -0500

    initial commit

--- src/service/socket.ts
+++ src/service/socket.ts
@@ -1,7 +1,7 @@
 import { io } from "socket.io-client";
 
-//const socket = io('http://localhost:5002',  { path: "/api/v2/socket/"})
-const socket = io("http://localhost:3002", {
+//const socket = io('http://localhost:3002',  { path: "/api/v2/socket/"})
+const socket = io("https://melife-backend-production.up.railway.app", {
   path: "/api/socket/",
 });
 
